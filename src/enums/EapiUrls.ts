18f8cd8db780c4234a66c775f08ea8fe0885bb47
CiaranMerritt <ciaran.merritt@colliewobble.com>
2022-03-19 20:35:49 +0000

    feat: Initial project setup

---
+++ src/enums/EapiUrls.ts
@@ -0,0 +1,7 @@
+
+enum APIUrls { 
+    BaseAPIUrl = "http://localhost:14434/api",
+    PhotosUrl = "http://localhost:14434/photos"
+}
+
+export default APIUrls;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
+import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-  <router-view></router-view>
+    <h1>About</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
 </template>
 
 <style>
