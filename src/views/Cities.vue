260da835e6a66df58d0b76f03b0fd40179ce5064
CiaranMerritt <ciaran.merritt@colliewobble.com>
2022-03-20 19:51:07 +0000

    feat: Adding a filter search

--- src/views/Cities.vue
+++ src/views/Cities.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import axios from 'axios'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
 //Components
 import HelloWorld from '../components/HelloWorld.vue'
@@ -16,13 +16,16 @@ import ISavedCity from '../interfaces/isavedCity';
 
 
 const savedCities = ref<Array<ISavedCity>>([])
-const searchedCities = ref<Array<any>>([])
+const searchedCities = ref<Array<any>>([]);
+const filterSearch = ref<string>("");
+const filteredSearchedCities = computed(() => searchedCities.value.filter((city) => { return city.city.toUpperCase().includes(filterSearch.value.toUpperCase()) }))
 const modalTitle = ref<string>("Modal title");
 const modalSearchCountryCode = ref<string>("");
 const cityName = ref<string>("");
 const countryName = ref<string>("");
 const lastUpdated = ref<string>("");
 const parameters = ref<string>("");
+const isWaitingForResponse = ref<boolean>(false);
 
 
 function refreshData() {
@@ -37,18 +40,21 @@ function searchCities() {
         const config = {
 
             withCredentials: false,
-            validateStatus: () => {
-                return true; // I'm always returning true, you may want to do it depending on the status received
+            validateStatus: (status: number) => {
+                return status < 500;
             },
 
         };
+        isWaitingForResponse.value = true;
         axios.get('https://docs.openaq.org/v2/cities?limit=1000&page=1&offset=0&sort=asc&country_id=' + modalSearchCountryCode.value + '&order_by=city', config)
             .then((response: any) => {
                 searchedCities.value = [];
                 searchedCities.value.push(...response.data.results);
                 console.log(response.data.results);
                 console.log(response)
+                isWaitingForResponse.value = false;
             }).catch((error) => {
+                isWaitingForResponse.value = false;
                 console.log(error);
             });
     }
@@ -170,9 +176,38 @@ onMounted(() => {
                         <input type="text" class="form-control" v-model="modalSearchCountryCode" />
                     </div>
 
-                    <button type="button" class="btn btn-primary" @click="searchCities()">Search</button>
+                    <button
+                        type="button"
+                        class="btn btn-primary m-2"
+                        @click="searchCities()"
+                        :disabled="isWaitingForResponse"
+                    >Search</button>
 
-                    <div class="tableFixHead">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Filter</span>
+                        <input type="text" class="form-control" v-model="filterSearch" />
+                    </div>
+
+                    <div v-if="isWaitingForResponse">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-arrow-repeat svgSpinner"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"
+                            />
+                            <path
+                                fill-rule="evenodd"
+                                d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
+                            />
+                        </svg>
+                    </div>
+
+                    <div class="tableFixHead" v-if="searchedCities.length > 0">
                         <table>
                             <thead>
                                 <tr>
@@ -185,7 +220,7 @@ onMounted(() => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="(searchedCity, index) in searchedCities">
+                                <tr v-for="(searchedCity, index) in filteredSearchedCities">
                                     <td>{{ index }}</td>
                                     <td>{{ searchedCity.country }}</td>
                                     <td>{{ searchedCity.city }}</td>
@@ -223,7 +258,7 @@ onMounted(() => {
 <!-- 
     {"country":"GB","city":"Aberdeen","count":161282393,"locations":3,"firstUpdated":"2016-02-27T20:00:00+00:00","lastUpdated":"2021-02-01T12:00:00+00:00","parameters":["no2","o3","pm10","pm25"]}
      -->
-<style>
+<style scoped>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -254,5 +289,22 @@ td {
 }
 th {
     background: #eee;
+}
+
+.svgSpinner {
+    top: 50%;
+    left: 50%;
+    width: 5%;
+    height: 5%;
+    animation-name: spin;
+    animation-duration: 1000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+}
+
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
 }
 </style>
